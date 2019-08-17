5446dbd2708920ddafa03174e4ed0e4e1f4ad1d1
littleyellow <450047998@qq.com>
2019-08-17 16:38:07 +0800

    m

--- app/src/main/java/com/littleyellow/demo/TestMainActivity.java
+++ app/src/main/java/com/littleyellow/demo/TestMainActivity.java
@@ -36,7 +36,7 @@ public class TestMainActivity extends AppCompatActivity {
             }
         });
 
-        String master = "M"+1+2+3;
+        String master = "M"+1+2+3+4;
 
         String test = "A"+1;
     }
