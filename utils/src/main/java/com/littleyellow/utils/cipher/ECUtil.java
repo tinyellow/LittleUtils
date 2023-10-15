5b52918817050f7c81cf930560fc1692153b8e19
黄春宁 <450047998@qq.com>
2023-10-16 00:03:14 +0800

    ECUtil

--- utils/src/main/java/com/littleyellow/utils/cipher/ECUtil.java
+++ utils/src/main/java/com/littleyellow/utils/cipher/ECUtil.java
@@ -7,11 +7,15 @@ import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1EncodableVector;
 import org.bouncycastle.asn1.ASN1Integer;
 import org.bouncycastle.asn1.DERSequence;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.encoders.Base64Encoder;
 import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemReader;
 import org.bouncycastle.util.io.pem.PemWriter;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.math.BigInteger;
@@ -24,6 +28,8 @@ import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Security;
 import java.security.Signature;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.ECGenParameterSpec;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
@@ -186,4 +192,33 @@ public class ECUtil {
         str.close();
         return str.toString();
     }
+
+
+    public RSAPublicKey readPublicKey(File file) throws Exception {
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+
+        try (FileReader keyReader = new FileReader(file);
+             PemReader pemReader = new PemReader(keyReader)) {
+
+            PemObject pemObject = pemReader.readPemObject();
+            byte[] content = pemObject.getContent();
+            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(content);
+            return (RSAPublicKey) factory.generatePublic(pubKeySpec);
+        }
+    }
+
+    public RSAPrivateKey readPrivateKey(File file) throws Exception {
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+
+        try (FileReader keyReader = new FileReader(file);
+             PemReader pemReader = new PemReader(keyReader)) {
+
+            PemObject pemObject = pemReader.readPemObject();
+            byte[] content = pemObject.getContent();
+            PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(content);
+            return (RSAPrivateKey) factory.generatePrivate(privKeySpec);
+        }
+    }
+
+
 }
